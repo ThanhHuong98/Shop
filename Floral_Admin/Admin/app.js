c3a5185537f63637f090d73fc98fb05b461eb607
Phu Duy <tppduy.hcmus@gmail.com>
2019-04-20 16:55:34 +0700

    [u] Add page order + user

--- Floral_Admin/Admin/app.js
+++ Floral_Admin/Admin/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var userRouter = require('./routes/user');
+var orderRouter = require('./routes/order');
 var app = express();
 
 // view engine setup
@@ -20,8 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- Floral_Admin/Admin/routes/index.js
+++ Floral_Admin/Admin/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express' });
 });
-
+router.get('/user', function(req, res, next) {
+  res.render('pages/user');
+});
+router.get('/order', function(req, res, next) {
+  res.render('pages/order');
+});
 module.exports = router;
