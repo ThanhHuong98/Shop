63c188e00cbe10b471c881bb2db6efc6fa0e5767
Huanzyz <mywayjjjj@gmail.com>
2019-06-04 13:48:06 +0700

    [passport & ajax] fix connection string

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var usersRouter = require('./routes/users');
 
 var db = require('./db')
 
-db.connect('mongodb://localhost:27017/ShopDB', function(err, db) {
+db.connect('mongodb://admin:1234@floralcluster-shard-00-00-g48mu.mongodb.net:27017,floralcluster-shard-00-01-g48mu.mongodb.net:27017,floralcluster-shard-00-02-g48mu.mongodb.net:27017/test?ssl=true&replicaSet=FloralCluster-shard-0&authSource=admin&retryWrites=true', function(err, db) {
   if (err) {
     console.log('Unable to connect to Mongo.')
     process.exit(1)
